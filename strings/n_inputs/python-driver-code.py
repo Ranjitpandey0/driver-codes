fad518cee51678721a2ce9186688aaae66a99cd9
Ranjitpandey0 <104191409+Ranjitpandey0@users.noreply.github.com>
2022-04-22 13:16:09 +0530

    Update python-driver-code.py

--- strings/n_inputs/python-driver-code.py
+++ strings/n_inputs/python-driver-code.py
@@ -1,7 +1,21 @@
 # Python Driver Code
 
 def solve(opening_time: str, duration: str) -> str:
-  # Your code goes here
+  def solve(start_time, total_duration):
+    ending_hr = (start_time[0] +total_duration[0]) % 24
+    ending_minute = (start_time[1] + total_duration[1])
+    if ending_minute > 59:
+        ending_hr +=1
+        if ending_hr>=24:
+            ending_hr -=24
+        ending_minute -=60
+    print(ending_hr, ending_minute)
+    
+n = int(input())
+for i in range(n):
+    start_time = list(map(int, input().split(" ")))
+    total_duration = list(map(int, input().split(" ")))
+    solve(start_time, total_duration)
 
   return 
 
